2d269ffc05619b8fd5aa700dc488f08f8dbf4c04
adishar93 <adishar93@gmail.com>
2020-07-19 17:22:13 +0530

    Implemented Firebase Authentication Sign In

---
+++ app/src/main/java/com/adishar93/moneytransactionapp/HomeActivity.java
@@ -0,0 +1,14 @@
+package com.adishar93.moneytransactionapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
