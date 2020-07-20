26697de8fee5749221331d4dd643c833d884ba22
adishar93 <adishar93@gmail.com>
2020-07-21 03:03:35 +0530

    Interface for MakeRequestUserDetailFragment is completed

--- app/src/main/java/com/adishar93/moneytransactionapp/fragments/MakeRequestUserDetailFragment.java
+++ app/src/main/java/com/adishar93/moneytransactionapp/fragments/MakeRequestUserDetailFragment.java
@@ -4,12 +4,22 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.adishar93.moneytransactionapp.R;
+import com.adishar93.moneytransactionapp.pojo.User;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 
 public class MakeRequestUserDetailFragment extends Fragment {
@@ -19,6 +29,14 @@ public class MakeRequestUserDetailFragment extends Fragment {
 
     private String mName;
     private String mEmail;
+
+    private Button mSendRequestButton;
+    private TextInputEditText mAmountEditText;
+    private TextInputEditText mDescriptionEditText;
+
+    FirebaseAuth mAuth;
+    private DatabaseReference mDatabaseStore;
+    private DatabaseReference mDatabaseRetrieve;
 
 
     public MakeRequestUserDetailFragment() {
@@ -42,6 +60,11 @@ public class MakeRequestUserDetailFragment extends Fragment {
             mName = getArguments().getString(ARG_PARAM1);
             mEmail = getArguments().getString(ARG_PARAM2);
         }
+
+        mAuth=FirebaseAuth.getInstance();
+
+        mDatabaseStore=FirebaseDatabase.getInstance().getReference("Requests");
+        mDatabaseRetrieve=FirebaseDatabase.getInstance().getReference("Users").child(mAuth.getUid());
     }
 
     @Override
@@ -52,6 +75,43 @@ public class MakeRequestUserDetailFragment extends Fragment {
         //initializing TextViews
         TextView mNameTextView=view.findViewById(R.id.tvName);
         TextView mEmailTextView=view.findViewById(R.id.tvEmail);
+        mSendRequestButton=view.findViewById(R.id.bSendRequest);
+        mAmountEditText=view.findViewById(R.id.tietAmount);
+        mDescriptionEditText=view.findViewById(R.id.tietDescription);
+
+        mSendRequestButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v) {
+                String amount=mAmountEditText.getText().toString();
+                String description=mAmountEditText.getText().toString();
+
+
+//                ValueEventListener postListener = new ValueEventListener() {
+//                    @Override
+//                    public void onDataChange(DataSnapshot dataSnapshot) {
+//                        // Get Post object and use the values to update the UI
+//
+//                        User currentUser=dataSnapshot.getValue(User.class);
+//
+//                        // ...
+//                    }
+//
+//                    @Override
+//                    public void onCancelled(DatabaseError databaseError) {
+//                        // Getting Post failed, log a message
+//                        Log.w("Firebase : ", "loadPost:onCancelled", databaseError.toException());
+//                        // ...
+//                    }
+//                };
+//                mDatabaseRetrieve.addValueEventListener(postListener);
+
+            }
+        });
+
+
+
+    //Setting Argument passed Values
         mNameTextView.setText(mName);
         mEmailTextView.setText(mEmail);
         return view;
