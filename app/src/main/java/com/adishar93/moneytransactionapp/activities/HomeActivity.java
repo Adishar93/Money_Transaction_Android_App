52c5e4517c1bb8c047a2367a3a7419e6ca40b4c3
adishar93 <adishar93@gmail.com>
2020-07-20 20:39:30 +0530

    Added HomeTabFragment as the child of HomeActivity and as the parent  for the tabbed Fragments

--- app/src/main/java/com/adishar93/moneytransactionapp/activities/HomeActivity.java
+++ app/src/main/java/com/adishar93/moneytransactionapp/activities/HomeActivity.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.viewpager2.adapter.FragmentStateAdapter;
 import androidx.viewpager2.widget.ViewPager2;
 
@@ -15,6 +16,8 @@ import android.transition.Fade;
 import android.view.Window;
 
 import com.adishar93.moneytransactionapp.R;
+import com.adishar93.moneytransactionapp.fragments.HomeTabFragment;
+import com.adishar93.moneytransactionapp.fragments.LoginFragment;
 import com.adishar93.moneytransactionapp.fragments.RecieveGrantFragment;
 import com.adishar93.moneytransactionapp.fragments.RequestMoneyFragment;
 import com.google.android.material.tabs.TabLayout;
@@ -23,24 +26,14 @@ import com.google.firebase.auth.FirebaseAuth;
 
 public class HomeActivity extends AppCompatActivity {
 
-    ViewPager2 mViewPager;
-    TabLayout mTabLayout;
-
     FirebaseAuth mAuth;
-
-    String[] mTabNames={"Request","Grant"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        //Initializing views
-        mViewPager=findViewById(R.id.view_pager);
-        mTabLayout=findViewById(R.id.tab_layout);
 
-        //Setting  up ViewPager and Tab Layout
-        initTabViewPager();
 
         //Firebase Variables
         mAuth=FirebaseAuth.getInstance();
@@ -52,44 +45,14 @@ public class HomeActivity extends AppCompatActivity {
             finish();
             startActivity(intent);
         }
+
+        //Load HomeTabFragment
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.replace(R.id.fragment_placeholder, HomeTabFragment.newInstance());
+        ft.commit();
     }
 
-    private void initTabViewPager()
-    {
-        mViewPager.setAdapter(new ViewPagerFragmentAdapter(this));
 
-        TabLayoutMediator.TabConfigurationStrategy tcs=new TabLayoutMediator.TabConfigurationStrategy() {
-            @Override
-            public void onConfigureTab(@NonNull TabLayout.Tab tab, int position) {
-                tab.setText(mTabNames[position]);
-            }
-        };
 
-        new TabLayoutMediator(mTabLayout,mViewPager,tcs).attach();
-    }
 
-    private class ViewPagerFragmentAdapter extends FragmentStateAdapter
-    {
-        public ViewPagerFragmentAdapter(FragmentActivity fragmentActivity) {
-            super(fragmentActivity);
-        }
-
-        @NonNull
-        @Override
-        public Fragment createFragment(int position) {
-            switch (position) {
-                case 0:
-                    return RequestMoneyFragment.newInstance();
-                case 1:
-                    return RecieveGrantFragment.newInstance();
-            }
-            return RequestMoneyFragment.newInstance();
-
-        }
-
-        @Override
-        public int getItemCount() {
-            return mTabNames.length;
-        }
-    }
 }
---
+++ app/src/main/java/com/adishar93/moneytransactionapp/fragments/HomeTabFragment.java
@@ -0,0 +1,103 @@
+package com.adishar93.moneytransactionapp.fragments;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+import androidx.viewpager2.widget.ViewPager2;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.adishar93.moneytransactionapp.R;
+import com.adishar93.moneytransactionapp.activities.HomeActivity;
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
+import com.google.firebase.auth.FirebaseAuth;
+
+
+public class HomeTabFragment extends Fragment {
+
+    ViewPager2 mViewPager;
+    TabLayout mTabLayout;
+
+
+
+    String[] mTabNames={"Request","Grant"};
+
+    public HomeTabFragment() {
+        // Required empty public constructor
+    }
+
+
+    public static HomeTabFragment newInstance() {
+        HomeTabFragment fragment = new HomeTabFragment();
+
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+       View view= inflater.inflate(R.layout.fragment_home_tab, container, false);
+
+        //Initializing views
+        mViewPager=view.findViewById(R.id.view_pager);
+        mTabLayout=view.findViewById(R.id.tab_layout);
+
+        //Setting  up ViewPager and Tab Layout
+        initTabViewPager();
+
+       return view;
+    }
+
+    private void initTabViewPager()
+    {
+        mViewPager.setAdapter(new ViewPagerFragmentAdapter(this));
+
+        TabLayoutMediator.TabConfigurationStrategy tcs=new TabLayoutMediator.TabConfigurationStrategy() {
+            @Override
+            public void onConfigureTab(@NonNull TabLayout.Tab tab, int position) {
+                tab.setText(mTabNames[position]);
+            }
+        };
+
+        new TabLayoutMediator(mTabLayout,mViewPager,tcs).attach();
+    }
+
+    private class ViewPagerFragmentAdapter extends FragmentStateAdapter
+    {
+        public ViewPagerFragmentAdapter(Fragment fragment) {
+            super(fragment);
+        }
+
+        @NonNull
+        @Override
+        public Fragment createFragment(int position) {
+            switch (position) {
+                case 0:
+                    return RequestMoneyFragment.newInstance();
+                case 1:
+                    return RecieveGrantFragment.newInstance();
+            }
+            return RequestMoneyFragment.newInstance();
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return mTabNames.length;
+        }
+    }
+}
