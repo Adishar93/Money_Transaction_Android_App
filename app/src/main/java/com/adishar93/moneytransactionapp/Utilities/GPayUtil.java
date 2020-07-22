e9e92e5beda84b49d8e6c8ffbcdafec973dbaf33
adishar93 <adishar93@gmail.com>
2020-07-22 18:22:03 +0530

    Created methods in GPayUtil file

--- app/src/main/java/com/adishar93/moneytransactionapp/Utilities/GPayUtil.java
+++ app/src/main/java/com/adishar93/moneytransactionapp/Utilities/GPayUtil.java
@@ -1,6 +1,128 @@
 package com.adishar93.moneytransactionapp.Utilities;
 
+import android.app.Activity;
+import android.os.Build;
+
+import androidx.annotation.RequiresApi;
+
+import com.google.android.gms.wallet.PaymentsClient;
+import com.google.android.gms.wallet.Wallet;
+import com.google.android.gms.wallet.WalletConstants;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Optional;
+
 public class GPayUtil {
 
+    private static JSONObject getBaseRequest() throws JSONException {
+        return new JSONObject().put("apiVersion", 2).put("apiVersionMinor", 0);
+    }
+
+    private static JSONObject getGatewayTokenizationSpecification() throws JSONException {
+        return new JSONObject() {{      put("type", "PAYMENT_GATEWAY");
+            put("parameters", new JSONObject() {
+                {
+                    put("gateway", "example");
+                    put("gatewayMerchantId", "exampleGatewayMerchantId");
+                }
+            });
+        }};
+    }
+
+    private static JSONArray getAllowedCardNetworks() {
+        return new JSONArray()
+
+                .put("MASTERCARD")
+                .put("VISA");
+    }
+
+    private static JSONArray getAllowedCardAuthMethods() {
+        return new JSONArray()
+                .put("PAN_ONLY")
+                .put("CRYPTOGRAM_3DS");
+    }
+
+
+    private static JSONObject getBaseCardPaymentMethod() throws JSONException {
+        JSONObject cardPaymentMethod = new JSONObject();
+        cardPaymentMethod.put("type", "CARD");
+
+        JSONObject parameters = new JSONObject();
+        parameters.put("allowedAuthMethods", getAllowedCardAuthMethods());
+        parameters.put("allowedCardNetworks", getAllowedCardNetworks());
+
+        cardPaymentMethod.put("parameters", parameters);
+
+        return cardPaymentMethod;
+    }
+
+    private static JSONObject getCardPaymentMethod() throws JSONException {
+        JSONObject cardPaymentMethod = getBaseCardPaymentMethod();
+        cardPaymentMethod.put("tokenizationSpecification", getGatewayTokenizationSpecification());
+
+        return cardPaymentMethod;
+    }
+
+    //PaymentsClient is used for interaction with the Google Pay API
+    public static PaymentsClient createPaymentsClient(Activity activity) {
+        Wallet.WalletOptions walletOptions =
+                new Wallet.WalletOptions.Builder().setEnvironment(WalletConstants.ENVIRONMENT_TEST).build();
+        return Wallet.getPaymentsClient(activity, walletOptions);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    public static Optional<JSONObject> getIsReadyToPayRequest() {
+        try {
+            JSONObject isReadyToPayRequest = getBaseRequest();
+            isReadyToPayRequest.put(
+                    "allowedPaymentMethods", new JSONArray().put(getBaseCardPaymentMethod()));
+
+            return Optional.of(isReadyToPayRequest);
+
+        } catch (JSONException e) {
+            return Optional.empty();
+        }
+    }
+
+    private static JSONObject getTransactionInfo(String price) throws JSONException {
+        JSONObject transactionInfo = new JSONObject();
+        transactionInfo.put("totalPrice", price);
+        transactionInfo.put("totalPriceStatus", "FINAL");
+       // transactionInfo.put("countryCode", Constants.COUNTRY_CODE);
+       // transactionInfo.put("currencyCode", Constants.CURRENCY_CODE);
+        transactionInfo.put("checkoutOption", "COMPLETE_IMMEDIATE_PURCHASE");
+
+        return transactionInfo;
+    }
+
+    private static JSONObject getMerchantInfo() throws JSONException {
+        return new JSONObject().put("merchantName", "Example Merchant");
+    }
+
+
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    public static Optional<JSONObject> getPaymentDataRequest(long inputPrice) {
+
+        final String price = String.valueOf(inputPrice);
+
+        try {
+            JSONObject paymentDataRequest = getBaseRequest();
+            paymentDataRequest.put(
+                    "allowedPaymentMethods", new JSONArray().put(getCardPaymentMethod()));
+            paymentDataRequest.put("transactionInfo",getTransactionInfo(price));
+            paymentDataRequest.put("merchantInfo", getMerchantInfo());
+
+
+
+            return Optional.of(paymentDataRequest);
+
+        } catch (JSONException e) {
+            return Optional.empty();
+        }
+    }
 
 }
