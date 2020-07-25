e4f34c4669254e7dd1c4f45a45ae8511d38011e6
adishar93 <adishar93@gmail.com>
2020-07-25 18:18:25 +0530

    Firebase Cloud Messaging Token now accessible and is stored in Database as soon as received

--- app/src/main/java/com/adishar93/moneytransactionapp/activities/HomeActivity.java
+++ app/src/main/java/com/adishar93/moneytransactionapp/activities/HomeActivity.java
@@ -1,18 +1,32 @@
 package com.adishar93.moneytransactionapp.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.adishar93.moneytransactionapp.R;
 import com.adishar93.moneytransactionapp.fragments.HomeTabFragment;
+import com.adishar93.moneytransactionapp.pojo.User;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.InstanceIdResult;
 
 public class HomeActivity extends AppCompatActivity {
 
     FirebaseAuth mAuth;
+    FirebaseInstanceId mInstanceId;
+    DatabaseReference mDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +37,8 @@ public class HomeActivity extends AppCompatActivity {
 
         //Firebase Variables
         mAuth=FirebaseAuth.getInstance();
+        mInstanceId=FirebaseInstanceId.getInstance();
+
 
         //Check Login Or Not
         if(mAuth.getCurrentUser()==null)
@@ -30,6 +46,39 @@ public class HomeActivity extends AppCompatActivity {
             Intent intent=new Intent(this,AuthenticationActivity.class);
             finish();
             startActivity(intent);
+        }
+        //If Logged in, get the FirebaseMessagingToken and Store in database for the user
+        else
+        {
+            mDatabase= FirebaseDatabase.getInstance().getReference("DeviceTokens").child(mAuth.getUid());
+            mInstanceId.getInstanceId().addOnCompleteListener(new OnCompleteListener<InstanceIdResult>() {
+                @Override
+                public void onComplete(@NonNull Task<InstanceIdResult> task) {
+                    if (!task.isSuccessful()) {
+                        Log.w("Firebase : ", "getInstanceId for device token failed", task.getException());
+
+                        return;
+                    }
+
+                    // Get new Instance ID token
+                    String token = task.getResult().getToken();
+                    mDatabase.setValue(token).addOnSuccessListener(new OnSuccessListener<Void>() {
+                        @Override
+                        public void onSuccess(Void aVoid) {
+                            Log.w("Firebase : ", "device token saved to database successfully!");
+
+                        }
+                    })
+                            .addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    Log.w("Firebase : ", "failed to save device token to database", e);
+
+                                }
+                            });
+
+                }
+            });
         }
 
         //Load HomeTabFragment
