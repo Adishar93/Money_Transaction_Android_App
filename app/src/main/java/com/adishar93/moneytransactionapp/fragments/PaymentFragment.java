1b620f0a8f3599422cd2db5456f0638db6b7ad17
adishar93 <adishar93@gmail.com>
2020-08-02 00:19:41 +0530

    Cleaned GPay API implementation

--- app/src/main/java/com/adishar93/moneytransactionapp/fragments/PaymentFragment.java
+++ app/src/main/java/com/adishar93/moneytransactionapp/fragments/PaymentFragment.java
@@ -15,10 +15,10 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.adishar93.moneytransactionapp.R;
-import com.adishar93.moneytransactionapp.Utilities.GPayUtil;
 import com.adishar93.moneytransactionapp.pojo.Request;
 import com.adishar93.moneytransactionapp.pojo.Transaction;
 import com.adishar93.moneytransactionapp.pojo.User;
@@ -60,7 +60,7 @@ public class PaymentFragment extends Fragment {
     // A client for interacting with the Google Pay API.
     private PaymentsClient paymentsClient;
 
-    private View mGooglePayButton;
+    private Button mPayButton;
     private TextView mAmountTextView;
 
     //Two places in database will be written
@@ -125,210 +125,50 @@ public class PaymentFragment extends Fragment {
 
     }
 
-    @RequiresApi(api = Build.VERSION_CODES.N)
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        View view= inflater.inflate(R.layout.fragment_payment, container, false);
-        initializeGPayButton(view);
-        mAmountTextView=view.findViewById(R.id.tvAmount);
 
+        View view= inflater.inflate(R.layout.fragment_payment, container, false);
+
+        mAmountTextView=view.findViewById(R.id.tvAmount);
         mAmountTextView.setText(mAmountTextView.getText().toString()+" "+mPrice);
 
-        paymentsClient = GPayUtil.createPaymentsClient(getActivity());
-        possiblyShowGooglePayButton();
+        initializePayButton(view);
+
         return view;
     }
 
-    private void initializeGPayButton(View view) {
 
 
-
-        // The Google Pay button is a layout file – take the root view
-        mGooglePayButton = view.findViewById(R.id.llgpay_button);
-        mGooglePayButton.setOnClickListener(
-                new View.OnClickListener() {
-                    @RequiresApi(api = Build.VERSION_CODES.N)
-                    @Override
-                    public void onClick(View view) {
-                        //Snackbar.make(getView(), "Google Pay Button Pressed!", Snackbar.LENGTH_SHORT).show();
-                        Log.d("GPayButton : ","Pressed");
-
-
-
-
-
-                        //Temporary operation performed here for testing, this code will be shifted to onActivityResult later
-//                        Transaction to=new Transaction(mRequest,String.valueOf(System.currentTimeMillis()));
-//                        mCurrToDatabase.push().setValue(to);
-//
-//                        Transaction from=new Transaction(mUser.getUid(),mUser.getName(),mUser.getEmail(),mRequest.getAmount(),mRequest.getDescription(),String.valueOf(System.currentTimeMillis()));
-//                        mReceiverFromDatabase.push().setValue(from);
-//
-//                        mRequestsDatabase.child(mRequest.getUid()).removeValue();
-//                        getFragmentManager().popBackStack(null, FragmentManager.POP_BACK_STACK_INCLUSIVE);
-//                        Snackbar.make(getView(), "Payment Successful!", Snackbar.LENGTH_SHORT).show();
-
-                        //Temporarily commented
-                        requestPayment(view);
-                    }
-                });
-    }
-
-
-    @RequiresApi(api = Build.VERSION_CODES.N)
-    public void requestPayment(View view) {
-
-        // Disables the button to prevent multiple clicks.
-        mGooglePayButton.setClickable(false);
-
-        // The price provided to the API should include taxes and shipping.
-        // This price is not displayed to the user.
-        Log.d("requestPayment","Called");
-
-            mPrice="20";
-            Optional<JSONObject> paymentDataRequestJson = GPayUtil.getPaymentDataRequest(Long.parseLong(mPrice));
-            if (!paymentDataRequestJson.isPresent()) {
-                Log.d("payentDataRequestJson","NotPresent");
-                return;
-            }
-
-            PaymentDataRequest request =
-                    PaymentDataRequest.fromJson(paymentDataRequestJson.get().toString());
-
-            // Since loadPaymentData may show the UI asking the user to select a payment method, we use
-            // AutoResolveHelper to wait for the user interacting with it. Once completed,
-            // onActivityResult will be called with the result.
-            if (request != null) {
-                Log.d("GPay request","Not Null"+request.toJson());
-                AutoResolveHelper.resolveTask(
-                        paymentsClient.loadPaymentData(request),
-                        Objects.requireNonNull(getActivity()), LOAD_PAYMENT_DATA_REQUEST_CODE);
-            }
-
-
-    }
-
-    @RequiresApi(api = Build.VERSION_CODES.N)
-    private void possiblyShowGooglePayButton() {
-
-        final Optional<JSONObject> isReadyToPayJson = GPayUtil.getIsReadyToPayRequest();
-        if (!isReadyToPayJson.isPresent()) {
-            Log.w("isReadyToPayJson", ".ispresent() failed");
-            return;
-        }
-
-        // The call to isReadyToPay is asynchronous and returns a Task. We need to provide an
-        // OnCompleteListener to be triggered when the result of the call is known.
-        IsReadyToPayRequest request = IsReadyToPayRequest.fromJson(isReadyToPayJson.get().toString());
-        Task<Boolean> task = paymentsClient.isReadyToPay(request);
-        task.addOnCompleteListener(getActivity(),
-                new OnCompleteListener<Boolean>() {
-                    @Override
-                    public void onComplete(@NonNull Task<Boolean> task) {
-                        if (task.isSuccessful()) {
-                            setGooglePayAvailable(task.getResult());
-                            Log.w("isReadyToPay succeeded", task.getResult().toString());
-                        } else {
-                            Log.w("isReadyToPay failed", task.getException());
-                        }
-                    }
-                });
-    }
-
-    private void setGooglePayAvailable(boolean available) {
-        if (available) {
-            mGooglePayButton.setVisibility(View.VISIBLE);
-        } else {
-            Snackbar.make(getView(), "Sorry! Google Pay Not Available!", Snackbar.LENGTH_SHORT).show();
-        }
-    }
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        Log.d("PaymentFragment : ","OnActivity Result Called");
-        switch (requestCode) {
-            // value passed in AutoResolveHelper
-            case LOAD_PAYMENT_DATA_REQUEST_CODE:
-                Log.d("PaymentFragment : ","OnActivity Result code LOAD_PAYMENT_DATA_REQUEST_CODE");
-
-                switch (resultCode) {
-
-                    case Activity.RESULT_OK:
-                        PaymentData paymentData = PaymentData.getFromIntent(data);
-                        handlePaymentSuccess(paymentData);
-                        Log.d("Payment Fragment : ","RESULT_OK Payment Data:"+paymentData.toJson());
-                        break;
-
-                    case Activity.RESULT_CANCELED:
-                        // The user cancelled the payment attempt
-                        Log.d("Payment Fragment : ","RESULT_CANCELED ");
-                        break;
-
-                    case AutoResolveHelper.RESULT_ERROR:
-                        Status status = AutoResolveHelper.getStatusFromIntent(data);
-                        //handleError(status.getStatusCode());
-                        Log.d("Payment Fragment : ","RESULT_ERROR"+status.getStatusCode()+" "+status.getStatusMessage());
-                        break;
-                }
-
-                // Re-enables the Google Pay payment button.
-                mGooglePayButton.setClickable(true);
-        }
+       //Handle payment transaction
     }
 
-    private void handlePaymentSuccess(PaymentData paymentData) {
+    public void initializePayButton(View view)
+    {
+        mPayButton=view.findViewById(R.id.bPay);
 
-        // Token will be null if PaymentDataRequest was not constructed using fromJson(String).
-        final String paymentInfo = paymentData.toJson();
-        if (paymentInfo == null) {
-            return;
-        }
+        mPayButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                //Temporary operation performed here for testing, this code will be shifted to onActivityResult later and be replaced by UPI Intent code
+                        Transaction to=new Transaction(mRequest,String.valueOf(System.currentTimeMillis()));
+                        mCurrToDatabase.push().setValue(to);
 
+                        Transaction from=new Transaction(mUser.getUid(),mUser.getName(),mUser.getEmail(),mRequest.getAmount(),mRequest.getDescription(),String.valueOf(System.currentTimeMillis()));
+                        mReceiverFromDatabase.push().setValue(from);
 
-        //Perform App Related Events
-        Transaction to=new Transaction(mRequest,String.valueOf(System.currentTimeMillis()));
-        mCurrToDatabase.push().setValue(to);
-
-        Transaction from=new Transaction(mUser.getUid(),mUser.getName(),mUser.getEmail(),mRequest.getAmount(),mRequest.getDescription(),String.valueOf(System.currentTimeMillis()));
-        mReceiverFromDatabase.push().setValue(from);
-
-        mRequestsDatabase.child(mRequest.getUid()).removeValue();
-        getFragmentManager().popBackStack(null, FragmentManager.POP_BACK_STACK_INCLUSIVE);
-        Snackbar.make(getView(), "Payment Successful!", Snackbar.LENGTH_SHORT).show();
+                        mRequestsDatabase.child(mRequest.getUid()).removeValue();
+                        getFragmentManager().popBackStack(null, FragmentManager.POP_BACK_STACK_INCLUSIVE);
+                        Snackbar.make(getView(), "Payment Successful!", Snackbar.LENGTH_SHORT).show();
 
 
-
-
-
-
-
-        try {
-            JSONObject paymentMethodData = new JSONObject(paymentInfo).getJSONObject("paymentMethodData");
-            // If the gateway is set to "example", no payment information is returned - instead, the
-            // token will only consist of "examplePaymentMethodToken".
-
-            final JSONObject tokenizationData = paymentMethodData.getJSONObject("tokenizationData");
-            final String tokenizationType = tokenizationData.getString("type");
-            final String token = tokenizationData.getString("token");
-
-            if ("PAYMENT_GATEWAY".equals(tokenizationType) && "examplePaymentMethodToken".equals(token)) {
-                new AlertDialog.Builder(getActivity())
-                        .setTitle("Warning")
-                        .setMessage("Gateway replace name example")
-                        .setPositiveButton("OK", null)
-                        .create()
-                        .show();
             }
-
-            final JSONObject info = paymentMethodData.getJSONObject("info");
-
-            // Logging token string.
-            Log.d("Google Pay token: ", token);
-
-        } catch (JSONException e) {
-            throw new RuntimeException("Payment Data Parsing Error");
-        }
+        });
     }
+
 }
